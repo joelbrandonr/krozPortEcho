13d7dda73f408eeb3676577ca70ef5387f25a33b
Isaiah <isaiah@various-and-sundry.com>
2025-03-04 13:15:37 -0500

    Adding a main file with comments to indicate what steps should be added.

---
+++ cycle1/main.py
@@ -0,0 +1,24 @@
+FP = [0] * 24
+Fast = chr(234)
+HideOpenWall = False
+LavaRate = 0
+GravOn = False
+GravRate = 0
+Sideways = False
+HideGems = False
+HideStairs = False
+HideOpenWall = False
+TreeRate = 0
+HideCreate = True
+MagicEWalls = True
+
+
+def main():
+    # Main menu stuff!!!
+
+    # Level loader!!!
+
+    # Player move stuff!!!
+
+if __name__ == "__main__":
+    main()
