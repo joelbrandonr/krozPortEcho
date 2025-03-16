afc3a886850b9ba7e3d6f066d7b20490d2bbb142
CMathurin192 <calebmathurin192@gmail.com>
2025-03-15 20:16:03 -0400

    Update screenFunctions.py

--- cycle1/screenFunctions.py
+++ cycle1/screenFunctions.py
@@ -29,6 +29,8 @@ class Screen:
         txt_background = len(text) * 8
         pygame.draw.rect(self.window, self.text_background, (self.cursor[0], self.cursor[1], txt_background, 16))
         self.window.blit(txt, self.cursor)
+        pygame.display.flip()
+
     
     def Writeln(self, text):
         txt = self.font.render(text, False, self.text_color)
@@ -41,6 +43,8 @@ class Screen:
         if cursor_y >= self.WINDOW_HEIGHT:
             cursor_y = 0 
         self.cursor = (cursor_x, cursor_y)
+        pygame.display.flip()
+
     
     def TextBackground(self, color):
         self.text_background = color
